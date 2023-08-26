3ee82a5d1104dc3b8ad460614c40f646c5ce25ca
Veeraphatlnwza <su.veeraphat_st@tni.ac.th>
2023-08-26 11:17:10 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import FlatList_HeaderFooter from './components/FlatList_HeaderFooter'
+import News from './components/News.Js'
+
 
 
 const App = () => {
   return (
     <View style={styles.container}>
-        <FlatList_HeaderFooter/>
+      <FlatList_HeaderFooter/>
+      <News/>
     </View>
   )
 }
